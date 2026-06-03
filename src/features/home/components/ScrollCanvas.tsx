f7a4dbdcf504682f615a067951a90edd11262e91
prakashshiromani <prakashmax18@gmail.com>
2026-06-03 20:11:07 +0530

    perf: resize background frames to 720p and convert to WebP to reduce asset size by 35%

--- src/features/home/components/ScrollCanvas.tsx
+++ src/features/home/components/ScrollCanvas.tsx
@@ -8,7 +8,7 @@ const FRAME_COUNT = 240;
 const CRITICAL_FRAMES = 8;
 
 function frameSrc(i: number) {
-    return `/images/1/ezgif-frame-${i.toString().padStart(3, "0")}.jpg`;
+    return `/images/1/ezgif-frame-${i.toString().padStart(3, "0")}.webp`;
 }
 
 export default function ScrollCanvas() {
